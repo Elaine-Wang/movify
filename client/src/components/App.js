2d6c975f4edfea9f12fab1242cad093667d00169
Elaine Wang <elainetraps15@gmail.com>
2019-12-02 23:56:35 -0600

    npm install

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css';
+
+import Home from './components/Home/Home.jsx';
+import Gallery from './components/Gallery/Gallery.jsx';
+import Detail from './components/Detail/Detail.jsx';
+
 import { Form } from 'semantic-ui-react'
 import axios from 'axios'
 
-export default class Create extends Component {
+//require('./styles/main.scss');
+
+export default class App extends Component {
+  // render() {
+  //   return (
+  //     <div className="nav">
+  //       <h1>HELLO</h1>
+  //       <Router>
+  //         <div>
+  //           <h1><Link to="/" className="links">MOVIES</Link> </h1>
+  //           <h2><Link to="/" className="links">SEARCH</Link> <Link to="/gallery" className="links">GALLERY</Link> </h2>
+  //         </div>
+  //       </Router>
+  //       <Router>
+  //         <Switch>
+  //           <Route exact path='/' component={Home} />,
+  //           <Route exact path='/gallery' component={Gallery} />,
+  //           <Route exact path='/detail' component={Detail} />
+  //         </Switch>
+  //       </Router>
+  //     </div>
+  //   );
+  // }
+
   constructor(props) {
     super(props)
     this.state = {
