0f2f3a695e237862e09667dbb3fa1e24d7c89152
Elaine Wang <elainetraps15@gmail.com>
2019-12-04 14:10:34 -0600

    gg

--- app.js
+++ app.js
@@ -38,53 +38,55 @@ mongoose.connect(dbRoute, function (err, client) {
         let db = mongoose.connection;
         db.once('open', () => console.log('connected to the database'));
 
-        var mapFunc = function () {
-            for (var idx1 = 0; idx1 < this.keywords.length; idx1++) {
-                for (var idx2 = idx1 + 1; idx2 < this.keywords.length; idx2++) {
-                    emit(this.keywords[idx1], this.keywords[idx2]);
-                    emit(this.keywords[idx2], this.keywords[idx1]);
-                }
-            }
-        };
-
-        var reduceFunc = function (keyKeywordId, keywords) {
-            if (keywords.length === 0)
-                return -1;
-            var modeMap = {};
-            var maxEl = keywords[0], maxCount = 1;
-            for (var i = 0; i < keywords.length; i++) {
-                var el = keywords[i];
-                if (modeMap[el] == null)
-                    modeMap[el] = 1;
-                else
-                    modeMap[el] += 1;
-                if (modeMap[el] === maxCount) {
-                    if (el < maxEl) {
-                        maxEl = el;
+        app.get('/trend_get', (req, res) => {
+            var mapFunc = function () {
+                for (var idx1 = 0; idx1 < this.keywords.length; idx1++) {
+                    for (var idx2 = idx1 + 1; idx2 < this.keywords.length; idx2++) {
+                        emit(this.keywords[idx1], this.keywords[idx2]);
+                        emit(this.keywords[idx2], this.keywords[idx1]);
                     }
                 }
-                if (modeMap[el] > maxCount) {
-                    maxEl = el;
-                    maxCount = modeMap[el];
+            };
+
+            var reduceFunc = function (keyKeywordId, keywords) {
+                if (keywords.length === 0)
+                    return -1;
+                var modeMap = {};
+                var maxEl = keywords[0], maxCount = 1;
+                for (var i = 0; i < keywords.length; i++) {
+                    var el = keywords[i];
+                    if (modeMap[el] == null)
+                        modeMap[el] = 1;
+                    else
+                        modeMap[el] += 1;
+                    if (modeMap[el] === maxCount) {
+                        if (el < maxEl) {
+                            maxEl = el;
+                        }
+                    }
+                    if (modeMap[el] > maxCount) {
+                        maxEl = el;
+                        maxCount = modeMap[el];
+                    }
                 }
-            }
-            return maxEl;
-        };
+                return maxEl;
+            };
 
-        var mapReduceRes = db.keywords.mapReduce(
-            mapFunc,
-            reduceFunc,
-            { out: "times" }
-        );
+            var mapReduceRes = db.keywords.mapReduce(
+                mapFunc,
+                reduceFunc,
+                { out: "times" }
+            );
 
-        var updateRes = db.times.update({}, { $rename: { "value": "keywordPair" } }, false, true)
-        db.times.find().toArray(function (err, docs) {
-            if (err) {
-                handleError(res, err.message, "Failed to get trends.");
-            } else {
-                res.json(docs);
-            }
-        });;
+            var updateRes = db.times.update({}, { $rename: { "value": "keywordPair" } }, false, true)
+            db.times.find().toArray(function (err, docs) {
+                if (err) {
+                    handleError(res, err.message, "Failed to get trends.");
+                } else {
+                    res.json(docs);
+                }
+            });;
+        })
     }
 
     // // checks if connection with the database is successful
